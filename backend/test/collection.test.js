39ede2bb7543d7fb75e49690149ba9788de661cd
Jamrod <myfavoritenoisemaker@gmail.com>
2020-01-27 16:31:59 -0700

    added test, none working yet

--- backend/test/collection.test.js
+++ backend/test/collection.test.js
@@ -2,9 +2,9 @@ const expect = require("chai").expect;
 const supertest = require("supertest");
 const api = supertest("http://localhost:4000");
 
-Describe("POST /", () => {
-    Before(done => {
-        api
-            .post("/api/collection/")
-    })
-})
+// Describe("POST /", () => {
+//     Before(done => {
+//         api
+//             .post("/api/collection/")
+//     })
+// })
--- backend/test/user.test..js
+++ backend/test/user.test..js
@@ -2,63 +2,91 @@ const expect = require("chai").expect;
 const supertest = require("supertest");
 const api = supertest("http://localhost:4000");
 
-Describe("POST /", () => {
-    let uid = ""
-    Before(done => {
+describe("GET /api/user/", () => {
+    it("Should return a 200 response", done => {
         api
-            .post("/api/user/")
+            .get("/")
             .set("Accept", "application/json")
-            .send({
-                "first-name": "Test",
-                "last-name": "Person",
-                "user-name": "testPerson",
-                "password": "password",
-                "email": "email@test.com"
-            })
-            .end(done)
-    })
-    it("Should add new user then find by user-name",
-        function (done) {
-            api
-                .get("/api/user/testPerson")
-                .set("Accept", "application/json")
-                .end((err, res) => {
-                    uid = res.body.body[res.body.length - 1]._id
-                    expect(res.body[res.body.length = 1]).to.include({
-                        "first-name": "Test",
-                        "last-name": "Person",
-                        "user-name": "testPerson",
-                        "password": "password",
-                        "email": "email@test.com"
-                    })
-                    done()
-                })
+            .expect(200, done);
+    });
+    it("should return an array", done => {
+        api
+            .get("/")
+            .set("Accept", "application/json")
+            .end((err, res) => {
+                expect(res.body).to.be.an("array");
+                done();
+            });
+    });
+    it("should return an array of objects that have a field called 'username'", done => {
+        api
+            .get("/")
+            .set("Accept", "application/json")
+            .end((err, res) => {
+                expect(res.body.every(i => i.username)).to.be.true;
+                done();
+            });
+    });
+});
 
-        })
+// describe("POST /", () => {
+//     let uid = ""
+//     before(done => {
+//         api
+//             .post("/api/user/")
+//             .set("Accept", "application/json")
+//             .send({
+//                 "first-name": "Test",
+//                 "last-name": "Person",
+//                 "user-name": "testPerson",
+//                 "password": "password",
+//                 "email": "email@test.com"
+//             })
+//             .end(done)
+//     })
+//     it("Should add new user then find by user-name",
+//         function (done) {
+//             api
+//                 .get("/api/user/testPerson")
+//                 .set("Accept", "application/json")
+//                 .end((err, res) => {
+//                     uid = res.body.body[res.body.length - 1]._id
+//                     expect(res.body[res.body.length = 1]).to.include({
+//                         "first-name": "Test",
+//                         "last-name": "Person",
+//                         "user-name": "testPerson",
+//                         "password": "password",
+//                         "email": "email@test.com"
+//                     })
+//                     done()
+//                 })
 
-    it("Should edit test user by id and return edit"),
-        function (done) {
-            api
-                .put(`/api.user/${uid}`)
-                .send({
-                    "first-name": "Test",
-                    "last-name": "Person",
-                    "user-name": "testPerson",
-                    "password": "password",
-                    "email": "email@different.com"
-                })
-                .end((err, res) => {
-                    uid = res.body.body[res.body.length - 1]._id
-                    expect(res.body[res.body.length - 1]).to.include({
-                        "first-name": "Test",
-                        "last-name": "Person",
-                        "user-name": "testPerson",
-                        "password": "password",
-                        "email": "email@different.com"
-                    })
-                    done()
-                })
+//         })
 
-        }
-})
+//     it("Should edit test user by id and return edit"),
+//         function (done) {
+//             consdole.log(uid)
+//             api
+//                 .put(`/api.user/${uid}`)
+//                 .send({
+//                     "first-name": "Test",
+//                     "last-name": "Person",
+//                     "user-name": "testPerson",
+//                     "password": "password",
+//                     "email": "email@different.com"
+//                 })
+//                 .end((err, res) => {
+//                     uid = res.body.body[res.body.length - 1]._id
+//                     expect(res.body[res.body.length - 1]).to.include({
+//                         "first-name": "Test",
+//                         "last-name": "Person",
+//                         "user-name": "testPerson",
+//                         "password": "password",
+//                         "email": "email@different.com"
+//                     })
+//                     done()
+//                 })
+
+//         }
+// })
 
