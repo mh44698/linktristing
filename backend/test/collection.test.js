3afd3b0fc93f1d2cd778d23f6724a40e49ba0929
Jamrod <myfavoritenoisemaker@gmail.com>
2020-01-28 12:42:49 -0700

    done with backend tests

--- backend/test/collection.test.js
+++
@@ -1,4 +0,0 @@
-const expect = require("chai").expect;
-const supertest = require("supertest");
-const api = supertest("http://localhost:8080");
-
--- backend/test/link-collection.test.js
+++ backend/test/link-collection.test.js
@@ -57,7 +57,34 @@ describe("GET /api/link/", () => {
     })
 })
 
-describe("POST /", () => {
+describe("GET /api/collection/", () => {
+    it("Should return a 200 response", done => {
+        api
+            .get("/api/collection")
+            .set("Accept", "application/json")
+            .expect(200, done)
+    })
+    it("should return an array", done => {
+        api
+            .get("/api/collection")
+            .set("Accept", "application/json")
+            .end((err, res) => {
+                expect(res.body).to.be.an("array")
+                done()
+            })
+    })
+    it("should return an array of objects that have a field called 'title'", done => {
+        api
+            .get("/api/collection")
+            .set("Accept", "application/json")
+            .end((err, res) => {
+                expect(res.body.every(i => i.title)).to.be.true;
+                done()
+            })
+    })
+})
+
+describe("POST / user, collection and link", () => {
 
     before(done => {
         api
@@ -187,30 +214,39 @@ describe("PUT /api/collection/:id", () => {
     })
 })
 
-// describe("DELETE /api/user/:id", () => {
-//     let testId
-//     before(done => {
-//         api
-//             .get("/api/user/name/testPerson")
-//             .set("Accept", " application/json")
-//             .end((err, res) => {
-//                 testId = res.body._id
-//                 done()
-//             })
-//     })
-//     before(done => {
-//         api
-//             .delete(`/api/user/${testId}`)
-//             .set("Accept", "application/json")
-//             .end(done)
-//     })
-//     it("should delete the object from the array based on id", done => {
-//         api
-//             .get("/api/user")
-//             .set("Accept", "application/json")
-//             .end((err, res) => {
-//                 expect(res.body[res.body.length - 1]._id).to.not.include(testId)
-//                 done()
-//             })
-//     })
-// })
+describe("DELETE / user, collection and link", () => {
+    before(done => {
+        const lid = getLinkId()
+        api
+            .delete(`/api/link/${lid}`)
+            .set("Accept", "application/json")
+            .expect(res => { res.body._id = lid })
+            .end(done)
+
+    })
+    before(done => {
+        const cid = getColId()
+        api
+            .delete(`/api/collection/${cid}`)
+            .set("Accept", "application/json")
+            .expect(res => { res.body._id = cid })
+            .end(done)
+    })
+    before(done => {
+        const uid = getUId()
+        api
+            .delete(`/api/user/${uid}`)
+            .set("Accept", "application/json")
+            .end(done)
+    })
+    it("should delete the link, user and collection based on id", done => {
+        const uid = getUId()
+        api
+            .get("/api/user")
+            .set("Accept", "application/json")
+            .end((err, res) => {
+                expect(res.body[res.body.length - 1]._id).to.not.include(uid)
+                done()
+            })
+    })
+})
